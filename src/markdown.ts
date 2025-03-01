d7b8770178fe2c4adc540580c73c67111e34d70c
gincheong2 <gincheong2@gmail.com>
2025-03-01 21:24:45 +0900

    feat: 페이지 타이틀 및 nav 추가

--- src/markdown.ts
+++ src/markdown.ts
@@ -3,8 +3,14 @@ import Showdown from 'showdown';
 const converter = new Showdown.Converter();
 converter.setOption('noHeaderId', true);
 
+const routes = ['articles', 'about'];
+
 export function makeHtml(fileName: string, contents: string) {
   const html = converter.makeHtml(contents);
 
-  return `<title>2-19 / articles / ${fileName}</title>\n${html}`;
+  const title = `<title>2-19 / articles / ${fileName}</title>`;
+  const anchors = routes.map((route) => `<a href="/${route}">${route}</a>`).join('\n');
+  const nav = `<header><nav>\n${anchors}\n</nav></header>`;
+
+  return [title, nav, html].join('\n');
 }
