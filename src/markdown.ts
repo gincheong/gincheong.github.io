c14e6f08f72dbfca11132eb3fac0b8bd02691657
gincheong2 <gincheong2@gmail.com>
2025-03-01 21:44:49 +0900

    feat: 루트 경로 링크 추가

--- src/markdown.ts
+++ src/markdown.ts
@@ -6,13 +6,26 @@ converter.setOption('simpleLineBreaks', true);
 converter.setOption('tables', true);
 converter.setOption('strikethrough', true);
 
-const routes = ['articles', 'about'];
+const Routes = [
+  {
+    name: 'home',
+    path: '/',
+  },
+  {
+    name: 'articles',
+    path: '/articles',
+  },
+  {
+    name: 'about',
+    path: '/about',
+  },
+];
 
 export function makeHtml(fileName: string, contents: string) {
   const html = converter.makeHtml(contents);
 
   const title = `<title>2-19 / articles / ${fileName}</title>`;
-  const anchors = routes.map((route) => `<a href="/${route}">${route}</a>`).join('\n');
+  const anchors = Routes.map(({ name, path }) => `<a href="${path}">${name}</a>`).join('\n');
   const nav = `<header><nav>\n${anchors}\n</nav></header>`;
 
   return [title, nav, html].join('\n');
