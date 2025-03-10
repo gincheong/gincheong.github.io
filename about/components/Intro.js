6425e5c866d219344889bb8b7727d9510989eb05
gincheong2 <gincheong2@gmail.com>
2025-03-10 15:32:38 +0900

    feat: intro 소개 내용 수정

--- about/components/Intro.js
+++ about/components/Intro.js
@@ -6,29 +6,23 @@ class Intro extends HTMLElement {
     this.shadowRoot.innerHTML = `
       <link rel="stylesheet" href="/styles/base.css" />
       <style>
-        p {
+        li {
           font-size: 0.9rem;
         }
       </style>
       <section>
-        <h4>초기 개발부터 서비스 배포 및 운영 경험</h4>
+        <h4>사내 신 기술 도입에 기여</h4>
         <ul>
-          <li>
-            모바일 앱 서비스 Dr.Listen를 초기부터 개발하여 모바일 환경에서의 배포 및 운영 담당
-          </li>
-          <li>
-            React Native를 활용하여 React 생태계에 대한 전반적인 경험 축적
-          </li>
+          <li>회사 내 프로젝트에 최초로 GraphQL을 도입하여 사용함</li>
+          <li>기존 사용 중이던 REST API + Postman 방식과 비교하여 장/단점 분석</li>
+          <li>직접 사용한 경험을 통해 동료들과 의견을 나누며 기술 도입에 긍정적 영향을 줌</li>
         </ul>
 
-        <h4>asdasd</h4>
+        <h4>서비스 초기 개발부터 배포까지의 경험</h4>
         <ul>
-          <li>
-            123
-          </li>
-          <li>
-            234
-          </li>
+          <li>모바일 앱 Dr.Listen를 초기부터 개발하여 모바일 환경에서의 배포 및 운영 담당</li>
+          <li>React Native를 활용하여 iOS, Android 두 플랫폼 동시 개발</li>
+          <li>서비스를 성공적으로 런칭하고 사내 신규 프로젝트에 React Native가 도입되는 데 기여</li>
         </ul>
       </section> 
     `;
