0289f8597c4d5f08a404d1c070c4768357cc2f3d
gincheong2 <gincheong2@gmail.com>
2025-12-31 00:09:05 +0900

    feat: abount에 vim-playground 추가

--- about/components/Projects.js
+++ about/components/Projects.js
@@ -26,6 +26,26 @@ class Projects extends HTMLElement {
         <ul>
           <li>2019.03 ~ 2023.08 AWS를 통해 서비스</li>
           <li>335개 사용자 데이터 DB에 등록</li>
+
+        </ul>
+
+        <h3>
+          Vim Playground -
+          <a target="_blank" href="https://vim-playground-nine.vercel.app/">
+            Link
+          </a>
+          <small>
+          <a target="_blank" href="https://github.com/gincheong/vim-playground">
+            (github)
+          </a>
+          </small>
+        </h3>
+        <p>
+          Vim 키맵핑을 테스트해볼 수 있는 Playground 웹 서비스
+        </p>
+        <ul>
+          <li>Gemini 3 Pro 모델을 이용한 바이브 코딩</li>
+          <li>Vercel을 통해 서비스
         </ul>
       </section>
     `;
